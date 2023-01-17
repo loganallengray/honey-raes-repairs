eedc19bb509f81316aaa65547bc2d5f136599d0b
Logan Gray <loganallengray@gmail.com>
2023-01-17 15:40:28 -0600

    List all tickets completed

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,4 +1,35 @@
-export const TicketList = () => {
-    return <h2>List of Tickets</h2>
-}
+import { useEffect, useState } from "react"
 
+export const TicketList = () => {
+    const [tickets, setTickets] = useState([])
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/serviceTickets`)
+                .then(response => response.json())
+                .then((ticketArray) => {
+                    setTickets(ticketArray)
+                })
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+
+    return <>
+        <h2>List of Tickets</h2>
+
+        <article className="tickets">
+            {
+                tickets.map(
+                    (ticket) => {
+                        return (
+                            <section key={ticket.id} className="ticket">
+                                <header>{ticket.description}</header>
+                                <footer>Emergency: {ticket.emergency ? "Y" : "N"}</footer>
+                            </section>
+                        )
+                    }
+                )
+            }
+        </article>
+    </>
+}
